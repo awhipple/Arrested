550880a06ea440ba57e6bf74f33425238af1f85d
awhipple <a.rgwhip@gmail.com>
2014-04-19 19:57:34 -0700

    Set up blank game field

---
+++ Arrested/src/arrested/Arrested.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrested;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+/**
+ *
+ * @author Aaron
+ */
+public class Arrested extends BasicGame {
+        
+    private static final int SCREEN_W = 1600, SCREEN_H = 900;
+    private static final int TARGET_FPS = 60;
+    
+    @Override
+    public void init(GameContainer gc) throws SlickException {
+        
+    }
+    
+    @Override
+    public void render(GameContainer gc, Graphics grphcs) throws SlickException {
+
+    }
+    
+    @Override
+    public void update(GameContainer gc, int i) throws SlickException {
+        
+    }
+    
+    public static void main(String[] args) {
+        try {
+            AppGameContainer app = new AppGameContainer(new Arrested());
+            app.setDisplayMode(SCREEN_W, SCREEN_H, false);
+            app.setTargetFrameRate(TARGET_FPS);
+            app.start();
+        } catch (SlickException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public Arrested() {
+        super("Arrested");
+    }
+    
+    private class TrominoOverlapException extends Exception {}
+}
